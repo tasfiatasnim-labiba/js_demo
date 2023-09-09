029de98726e69083ed03824217b8f1f72b1f97bc
Tasfia Tasnim Labiba <136629763+tasfiatasnim-labiba@users.noreply.github.com>
2023-09-09 20:20:28 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,59 @@
+//text-change
+function textChange(){
+
+	document.getElementById('h2').innerHTML="hello world";
+}
+
+//text-change
+function wordChange(){
+
+	document.getElementById('h3').innerHTML="hello world";
+}
+
+//text-change
+function docChange(){
+
+	document.getElementById('h4').innerHTML="hello world";
+}
+
+//text-change
+function classChange(){
+
+	document.getElementById('h5').innerHTML="hello world";
+}
+
+//text-change
+function forChange(){
+
+	document.getElementById('h6').innerHTML="hello world";
+}
+
+//text-change
+function goChange(){
+
+	document.getElementById('h7').innerHTML="hello world";
+}
+
+//text-change
+function whileChange(){
+
+	document.getElementById('h8').innerHTML="hello world";
+}
+
+//text-change
+function toChange(){
+
+	document.getElementById('h9').innerHTML="hello world";
+}
+
+//text-change
+function onChange(){
+
+	document.getElementById('h10').innerHTML="hello world";
+}
+
+//text-change
+function infoChange(){
+
+	document.getElementById('h11').innerHTML="hello world";
+}
